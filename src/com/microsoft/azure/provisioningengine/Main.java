e6a029fa635d292c4c262e7e434ba0112b30c4f4
scoriani <scoriani@microsoft.com>
2016-10-24 21:27:22 -0700

    fixed some stuff

--- src/com/microsoft/azure/provisioningengine/Main.java
+++ src/com/microsoft/azure/provisioningengine/Main.java
@@ -12,17 +12,17 @@ import java.util.Date;
 
 public class Main {
 
-    static DateFormat dateFormat = new SimpleDateFormat("MMM dd, yyyy K:mm:ss a");
-    static ProvisioningEngine provEngine;
+    static private DateFormat dateFormat = new SimpleDateFormat("MMM dd, yyyy K:mm:ss a");
+    static private ProvisioningEngine provEngine;
 
     public static void main(String[] args) throws IOException, InterruptedException, InvalidKeyException, StorageException, URISyntaxException {
         Date start = new Date();
 
         // Create Managed Disk volume
-//        provEngine.CreateVolume("osdiskds");
+        provEngine.CreateVolume("osdiskds");
 
         // Create Base VM Instance based on MD
-//        provEngine.CreateBaseVMInstance("basevm");
+        provEngine.CreateBaseVMInstance("basevm");
 
         // Customize VM Instance (e.g. add new data disk)
         // Generalize VM Instance
@@ -30,7 +30,7 @@ public class Main {
         // Capture VM Image
         provEngine.CaptureVMImage("basevm","osdiskds","imgfinalvm");
 
-//        provEngine.DeployTargetVMInstance("finalvm");
+        provEngine.DeployTargetVMInstance("finalvm");
 
         Date end = new Date();
         System.out.println(dateFormat.format(new Date()) + " INFO: Test execution (took " + ((end.getTime() - start.getTime()) / 1000.00) + " seconds) \n");
